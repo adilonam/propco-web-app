7f0d554411a60cd7f89fd83ffc20ec03a0d69844
Adil ABBADI <adil.abbadi.1996@gmail.com>
2024-07-07 14:02:29 +0100

    add telegram handler

---
+++ pages/api/telegram.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type TelegramMessage = {
+  message?: {
+    chat: {
+      id: number;
+    };
+    text: string;
+  };
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { message }: TelegramMessage = req.body;
+
+    if (message && message.text === '/start') {
+      const chatId = message.chat.id;
+      const responseMessage = 'Opening Google...';
+      const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`;
+
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: responseMessage,
+          reply_markup: {
+            inline_keyboard: [[
+              { text: 'Open Google', url: 'https://google.com' }
+            ]]
+          },
+        }),
+      });
+    }
+    
+    res.status(200).send('OK');
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
