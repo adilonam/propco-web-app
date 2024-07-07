412a85991ec77215acd4548e1c7ba05b6907400e
Adil ABBADI <adil.abbadi.1996@gmail.com>
2024-07-07 22:01:43 +0100

    telegram: change hello msg

--- pages/api/telegram.ts
+++ pages/api/telegram.ts
@@ -38,27 +38,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         });
 
         console.log('User created or found:', user);
+        // Respond to the Telegram message
+        await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            chat_id: message.chat.id, // Passing the original chatId without converting to BigInt
+            text: "🌟 Welcome to the Ultimate PROPCO Token App! 🌟\n\n🚀 Buy Propco on Bitmart 🌐\nInvest in the future today! Click the link below to buy your Propco tokens and be part of the revolution:\n👉 https://www.bitmart.com/trade/en-US?symbol=PROPCO_USDT\n\n📢 Join the Official Channel 🕸️\nStay updated with the latest news, updates, and community discussions. Join us on Telegram:\n👉 https://t.me/propco\n\n🎉 Get Rewarded 🎉\nEarn Propco for free! Participate in various activities and tasks to get rewarded. The more you engage, the more you earn!\n\n👯‍♂️ Refer a Friend\nSpread the word! When your friends buy Propco, you get rewarded in Propco. Double the benefits, double the fun. Let's create a community of like-minded investors!\n\n🌔 Take Us to the Moon! 🌕\nBe a part of our journey to the moon. Your support can take us higher than ever before. Join the movement and watch your investment soar!",
+            reply_markup: {
+              inline_keyboard: [[
+                { text: 'Open App', web_app: { url: `${process.env.DOMAIN}?id=${user.id}` } }
+              ]]
+            },
+         }),
+         
+        });
       } catch (error) {
         console.error('Error creating or finding user:', error);
         return res.status(500).json({ error: 'Internal Server Error' });
       }
 
-      // Respond to the Telegram message
-      await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          chat_id: message.chat.id, // Passing the original chatId without converting to BigInt
-          text: responseMessage,
-          reply_markup: {
-            inline_keyboard: [[
-              { text: 'Open App', web_app: { url: process.env.DOMAIN } }
-            ]]
-          },
-        }),
-      });
+
 
       return res.status(200).send('OK');
     } else {
