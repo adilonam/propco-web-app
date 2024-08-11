8858cb98a51f4ac18b11455392b806b37682f467
Adil ABBADI <adil.abbadi.1996@gmail.com>
2024-08-11 22:36:55 +0100

    add uniswap to buy propco

--- pages/api/telegram.ts
+++ pages/api/telegram.ts
@@ -67,7 +67,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           },
           body: JSON.stringify({
             chat_id: message.chat.id, // Passing the original chatId without converting to BigInt
-            text: "🌟 Welcome to the Ultimate PROPCO Token App! 🌟\n\n🚀 Buy Propco on Bitmart 🌐\nInvest in the future today! Click the link below to buy your Propco tokens and be part of the revolution:\n👉 https://www.bitmart.com/trade/en-US?symbol=PROPCO_USDT\n\n📢 Join the Official Channel 🕸️\nStay updated with the latest news, updates, and community discussions. Join us on Telegram:\n👉 https://t.me/propco\n\n🎉 Get Rewarded 🎉\nEarn Propco for free! Participate in various activities and tasks to get rewarded. The more you engage, the more you earn!\n\n👯‍♂️ Refer a Friend\nSpread the word! When your friends buy Propco, you get rewarded in Propco. Double the benefits, double the fun. Let's create a community of like-minded investors!\n\n🌔 Take Us to the Moon! 🌕\nBe a part of our journey to the moon. Your support can take us higher than ever before. Join the movement and watch your investment soar!",
+            text: "🌟 Welcome to the Ultimate PROPCO Token App! 🌟\n\n🚀 Buy Propco on Bitmart 🌐\nInvest in the future today! Click the link below to buy your Propco tokens and be part of the revolution:\n👉 https://www.bitmart.com/trade/en-US?symbol=PROPCO_USDT\n\n💱 Buy Propco on Uniswap 🌐\nPrefer decentralized exchanges? Purchase your Propco tokens on Uniswap:\n👉 https://app.uniswap.org/explore/pools/polygon/0xd3ed7dF705e8399BE84838861173dDFC8124c251\n\n📢 Join the Official Channel 🕸️\nStay updated with the latest news, updates, and community discussions. Join us on Telegram:\n👉 https://t.me/propco\n\n🎉 Get Rewarded 🎉\nEarn Propco for free! Participate in various activities and tasks to get rewarded. The more you engage, the more you earn!\n\n👯‍♂️ Refer a Friend\nSpread the word! When your friends buy Propco, you get rewarded in Propco. Double the benefits, double the fun. Let's create a community of like-minded investors!\n\n🌔 Take Us to the Moon! 🌕\nBe a part of our journey to the moon. Your support can take us higher than ever before. Join the movement and watch your investment soar!",
             reply_markup: {
               inline_keyboard: [[
                 { text: 'Open App', web_app: { url: `${process.env.DOMAIN}/${user.id}` } }
@@ -81,7 +81,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 { text: 'Invite a Friend', url: `https://t.me/share/url?url=https%3A%2F%2Ft.me%2FPropcoBot%3Fstart%3D${user.id}&text=Propco%20Tokens%20gets%20even%20more%20rewarding%21%20Get%20tokens%20for%20referring%20friends%21%20Join%20my%20squad%2C%20and%20let%27s%20double%20the%20fun%20(and%20earnings%20%F0%9F%92%AF)%21%20%F0%9F%9A%80` }
               ],
               [
-                { text: 'Buy Propco Tokens', url: 'https://www.bitmart.com/trade/en-US?symbol=PROPCO_USDT' }
+                { text: 'Buy Propco on Bitmart', url: 'https://www.bitmart.com/trade/en-US?symbol=PROPCO_USDT' }
+              ]
+              ,
+              [
+                { text: 'Buy Propco on Uniswap', url: 'https://app.uniswap.org/explore/pools/polygon/0xd3ed7dF705e8399BE84838861173dDFC8124c251' }
               ]
               ]
             },
